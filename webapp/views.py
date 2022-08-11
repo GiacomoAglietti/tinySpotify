ac0ecb563480c65a1f4d0a5c5e4132192315df4b
ElCampa <58786031+ElCampa@users.noreply.github.com>
2022-08-11 18:14:54 +0200

    Init album-select

--- webapp/views.py
+++ webapp/views.py
@@ -95,11 +95,11 @@ def get_playlist_selected(id_playlist_selected):
                 where(PlaylistSong.id_playlist == id_playlist_selected).
                 order_by(PlaylistSong.num_in_playlist)) 
 
-        select_favourite= (
+        """select_favourite= (
                 select((func.count(PlaylistSong.id_song)).label('isFav')).
                 where(PlaylistSong.id_song == stmt_song.id).
                 where(PlaylistSong.id_playlist == 1))
-
+        """
 
     
 
@@ -113,9 +113,9 @@ def get_playlist_selected(id_playlist_selected):
         playlist_name = local_session.execute(stmt_playlist).scalar()
         if (playlist_name==None):
                 playlist_name="Playlist not found"
-
-        songs_list = local_session.execute(stmt_song,select_favourite).all()
-
+        
+        songs_list = local_session.execute(stmt_song).all()
+        
         num_songs = 0
         tot_length = 0
 
@@ -135,22 +135,38 @@ def albums():
 @views.route('/artists')
 #@login_required
 def artists():
-        stmt = select(Artist)#.where(Playlist.id_user == id)
+        stmt = select(User)#.where(Playlist.id_user == id)
         artist_list = local_session.execute(stmt).scalars()
         return render_template("artists.html", artist_list = artist_list)
 
 
-@views.route('/album/<album_selected>')
+@views.route('/albums/<int:id_album_selected>')
 #@login_required
-def get_album_selected(id, album_selected):
-        stmt = (select(Song.title).
-                join(PlaylistSong, Song.id == PlaylistSong.id_song).
-                join(Playlist, Playlist.id == PlaylistSong.id_playlist).
-                where(Playlist.id_user == id and Playlist.name == album_selected).
-                order_by(PlaylistSong.num_order))
+def get_album_selected(id_album_selected):
+        stmt_album = (
+                select(Album.name).
+                where(Album.id == id_album_selected))
 
-        songs_list = local_session.execute(stmt).scalars().all()
-        return render_template("album.html", songs_list = songs_list)
+        stmt_song=(
+                select(Song.id, Song.title, Song.id_album, Song.length, Song.num_in_album, Album.name).
+                join(Album, Song.id_album == Album.id).
+                where(Album.id == id_album_selected).
+                order_by(Song.num_in_album)) 
+
+        album_name = local_session.execute(stmt_album).scalar()
+        if (album_name==None):
+                album_name="Album not found"
+
+        songs_list = local_session.execute(stmt_song).all()
+
+        num_songs = 0
+        tot_length = 0
+
+        for song in songs_list:
+                num_songs += 1
+                tot_length += song.length
+
+        return render_template("album-select.html", songs_list = songs_list, album_name = album_name, num_songs=num_songs, tot_length=tot_length, actual_playlist=id_album_selected)
 
 
 @views.route('/create-new-playlist')
